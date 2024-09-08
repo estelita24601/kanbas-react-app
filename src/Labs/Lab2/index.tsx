1b07d6b1e8153963acf943b05261c071c2e6fa90
Estelita <estelitac24401@hotmail.com>
2024-09-08 16:15:38 -0700

    adding due dates to labs

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,6 +2,7 @@ export default function Lab2(){
     return(
         <div>
             <h2>Lab 2</h2>
+            <p>due Thursday 10/3 at 9pm PST</p>
         </div>
     );
 }
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -2,6 +2,7 @@ export default function Lab3(){
     return(
         <div>
             <h2>Lab 3</h2>
+            <p>due Thursday 10/17 at 9pm PST</p>
         </div>
     );
 }

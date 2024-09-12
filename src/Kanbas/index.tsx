2d196a17aec02f1caa4b6cc034ddc0c6e36f6fa8
Estelita <estelitac24401@hotmail.com>
2024-09-12 14:19:59 -0700

    formatting

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,14 +1,14 @@
-import {Route, Routes, Navigate} from "react-router"
+import { Route, Routes, Navigate } from "react-router"
 import Account from "./Account/Account";
 
 export default function Kanbas() {
     return (
         <div id="wd-kanbas">
             <h1>Kanbas</h1>
-            <Account/>
+            <Account />
             <Routes>
-                <Route path="/" element={<Navigate to="Account"/>}/>
-                <Route path="/Account/*" element={<Account/>}/>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Account />} />
             </Routes>
         </div>
     );

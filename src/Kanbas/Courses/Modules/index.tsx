6e8f42d4c3509b5234daaf4607a3bdacc3b44408
Estelita <estelitac24401@hotmail.com>
2024-09-14 23:44:41 -0700

    fleshing out the modules page

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -15,8 +15,9 @@ export default function Modules() {
 
             {/*TODO: add the rest of stuff from canvas, readings slides etc*/}
             <ul id="wd-modules">
+                {/* module 1.1 */}
                 <li className="wd-module">
-                    <div className="wd-title">Week 1</div>
+                    <div className="wd-title">Week 1, Lecture 1 - Course Intro, Syllabus, Agenda</div>
                     <ul className="wd-lessons">
                         <li className="wd-lesson">
                             <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -25,10 +26,29 @@ export default function Modules() {
                                 <li className="wd-content-item">Learn what is Web Development</li>
                             </ul>
                         </li>
+
+                        <li className="wd-lesson">
+                            <span>READING</span>
+                            <ul>
+                                <li>Full Stack Developer - Chapter 1 - Introduction</li>
+                                <li>Fill Stack Developer - Chapter 2 - Creating User Interfaces with HTML</li>
+                            </ul>
+                        </li>
+
+                        <li className="wd-lesson">
+                            <span>SLIDES</span>
+                            <ul>
+                                <li>Introduction to Web Development</li>
+                                <li>Creating an HTTP server with Node.js</li>
+                                <li>Creating a React Application</li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
+                </li> <br />
+
+                {/* module 1.2 */}
                 <li className="wd-module">
-                    <div className="wd-title">Week 2</div>
+                    <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
                     <ul className="wd-lessons">
                         <li className="wd-lesson">
                             <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -37,8 +57,51 @@ export default function Modules() {
                                 <li className="wd-content-item">TODO</li>
                             </ul>
                         </li>
+
+                        <li className="wd-lesson">
+                            <span>READING</span>
+                            <ul className="wd-content">
+                                <li>TODO</li>
+                            </ul>
+                        </li>
+
+                        <li className="wd-lesson">
+                            <span>SLIDES</span>
+                            <ul className="wd-content">
+                                <li>TODO</li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
+                </li> <br />
+
+                {/* module 2 */}
+                <li className="wd-module">
+                    <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">LEARNING OBJECTIVES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">TODO</li>
+                                <li className="wd-content-item">TODO</li>
+                            </ul>
+                        </li>
+
+                        <li className="wd-lesson">
+                            <span>READING</span>
+                            <ul className="wd-content">
+                                <li>TODO</li>
+                            </ul>
+                        </li>
+
+                        <li className="wd-lesson">
+                            <span>SLIDES</span>
+                            <ul className="wd-content">
+                                <li>TODO</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li> <br />
+
             </ul>
         </div>
     );

9d4992264735c5d1d6673ce72a0f90b314e059a3
Estelita <estelitac24401@hotmail.com>
2024-09-16 21:11:27 -0700

    added github link to table of contents

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <li><a id="wd-github" href="https://github.com/estelita24601/kanbas-react-app">Github</a></li>
         </ul>
     );
 }

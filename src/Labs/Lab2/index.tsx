814804582c7dd38ac9d9e96c03b77e5c054e672b
Estelita <estelitac24401@hotmail.com>
2024-09-20 15:27:05 -0700

    finishing section 3.1 of lab 2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -107,76 +107,215 @@ export default function Lab2() {
                     Padded all around
                 </div>
             </div>
-
             <div id="wd-css-margins">
+                <h2>Margins</h2>
                 <div
-                    className="wd-margin-bottom wd-padded-top-left wd-border-fat wd-border-red wd-border-solid wd-bg-color-yellow"
-                >
-                    Margin bottom //red border yellow inside
+                    className="wd-margin-bottom
+                        wd-padded-top-left
+                        wd-border-fat wd-border-red
+                        wd-border-solid
+                        wd-bg-color-yellow">
+                    Margin bottom
                 </div>
-
                 <div
                     className="wd-margin-right-left
-                    wd-padded-bottom-right
-                    wd-border-fat wd-border-blue
-                    wd-border-solid
-                    wd-bg-color-yellow"
-                >Margin left right //blue border yellow inside</div>
-
+                        wd-padded-bottom-right
+                        wd-border-fat wd-border-blue
+                        wd-border-solid
+                        wd-bg-color-yellow">
+                    Margin left right
+                </div>
                 <div
                     className="wd-margin-all-around
-                    wd-padding-fat wd-border-fat
-                    wd-border-yellow
-                    wd-border-solid
-                    wd-bg-color-blue
-                    wd-fg-color-white"
-                >Margin all around //yellow border blue inside</div>
+                        wd-padding-fat wd-border-fat
+                        wd-border-yellow
+                        wd-border-solid
+                        wd-bg-color-blue
+                        wd-fg-color-white">
+                    Margin all around
+                </div>
             </div>
 
             {/* SECTION 3.1.11 - styling corners */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-borders">
+                <h3>Rounded corners</h3>
+                <p className="wd-rounded-corners-top wd-border-thin
+                wd-border-blue wd-border-solid wd-padding-fat">
+                    Rounded corners on the top
+                </p>
+                <p className="wd-rounded-corners-bottom
+     wd-border-thin wd-border-blue wd-border-solid
+     wd-padding-fat">
+                    Rounded corners at the bottom
+                </p>
+                <p className="wd-rounded-corners-all-around
+     wd-border-thin wd-border-blue wd-border-solid
+     wd-padding-fat">
+                    Rounded corners all around
+                </p>
+                <p className="wd-rounded-corners-inline
+     wd-border-thin wd-border-blue wd-border-solid
+     wd-padding-fat">
+                    Different rounded corners
+                </p>
+
             </div>
 
             {/* SECTION 3.1.12 - styling dimensions */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-dimensions">
+                <h2>Dimension</h2>
+                <div>
+                    <div className="wd-dimension-portrait wd-bg-color-yellow">
+                        Portrait
+                    </div>
+                    <div className="wd-dimension-landscape wd-bg-color-blue
+                    wd-fg-color-white">
+                        Landscape
+                    </div>
+                    <div className="wd-dimension-square wd-bg-color-red">
+                        Square</div>
+                </div>
             </div>
 
             {/* SECTION 3.1.13 - styling relative position */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-position-relative">
+                <h2>Relative</h2>
+                <div className="wd-bg-color-gray">
+                    <div className="wd-bg-color-yellow
+                    wd-dimension-portrait">
+                        <div className="wd-pos-relative-nudge-down-right">
+                            Portrait</div></div>
+                    <div className="wd-pos-relative-nudge-up-right
+                    wd-bg-color-blue wd-fg-color-white wd-dimension-landscape">
+                        Landscape</div>
+                    <div className="wd-bg-color-red wd-dimension-square">
+                        Square</div>
+                </div>
             </div>
 
             {/* SECTION 3.1.14 - styling absolute position */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-position-absolute">
+                <h2>Absolute position</h2>
+                <div className="wd-pos-relative">
+                    <div className="wd-pos-absolute-10-10
+                    wd-bg-color-yellow wd-dimension-portrait">
+                        Portrait</div>
+                    <div className="wd-pos-absolute-50-50
+                    wd-bg-color-blue wd-fg-color-white
+                    wd-dimension-landscape">
+                        Landscape</div>
+                    <div className="wd-pos-absolute-120-20
+                    wd-bg-color-red wd-dimension-square">
+                        Square</div>
+                </div><br /><br /><br /><br /><br /><br /><br />
             </div>
 
+
             {/* SECTION 3.1.15 - styling fixed position */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-position-fixed">
+                <h2>Fixed position</h2>
+                Checkout the blue square that says "Fixed position" stuck all the way on the right and half way down the page. It doesn't scroll with the rest of the page. Its position is "Fixed".
+                <div className="wd-pos-fixed
+                wd-dimension-square wd-bg-color-blue
+                wd-fg-color-white">
+                    Fixed position
+                </div>
             </div>
 
             {/* SECTION 3.1.16 - styling z index */}
-            <div>
-                {/* TODO */}
+            <div id="wd-z-index">
+                <h2>Z index</h2>
+                <div className="wd-pos-relative">
+                    <div className="wd-pos-absolute-10-10
+                    wd-bg-color-yellow wd-dimension-portrait">
+                        Portrait
+                    </div>
+                    <div className="wd-zindex-bring-to-front
+                    wd-pos-absolute-50-50 wd-dimension-landscape
+                    wd-bg-color-blue wd-fg-color-white">
+                        Landscape
+                    </div>
+                    <div className="wd-pos-absolute-120-20
+                    wd-bg-color-red wd-dimension-square">
+                        Square
+                    </div>
+                </div><br /><br /><br /><br /><br /><br /><br />
             </div>
 
             {/* SECTION 3.1.17 - floating images and content */}
-            <div>
-                {/* TODO */}
+            <div id="wd-float-divs">
+                <h2>Float</h2>
+                <div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                        Yellow </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                        Blue </div>
+                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                        Red </div>
+                    <img className="wd-float-right"
+                        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                    <div className="wd-float-done"></div>
+                </div>
             </div>
+
 
             {/* SECTION 3.1.18 - grid layout */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-grid-layout">
+                <div id="wd-css-left-right-layout">
+                    <h2>Grid layout</h2>
+                    <div className="wd-grid-row">
+                        <div className="wd-grid-col-half-page wd-bg-color-yellow">
+                            <h3>Left half</h3>
+                        </div>
+                        <div className="wd-grid-col-half-page wd-bg-color-blue wd-fg-color-white">
+                            <h3>Right half</h3>
+                        </div>
+                    </div>
+                </div>
+                <div id="wd-css-left-third-right-two-thirds" className="wd-grid-row">
+                    <div className="wd-grid-col-third-page wd-bg-color-green
+        wd-fg-color-white">
+                        <h3>Left third</h3>
+                    </div>
+                    <div className="wd-grid-col-two-thirds-page wd-bg-color-red wd-fg-color-white">
+                        <h3>Right two thirds</h3>
+                    </div>
+                </div>
+                <div id="wd-css-side-bars" className="wd-grid-row">
+                    <div className="wd-grid-col-left-sidebar wd-bg-color-yellow">
+                        <h3>Side bar</h3>
+                        <p>This is the left sidebar</p>
+                    </div>
+                    <div className="wd-grid-col-main-content wd-bg-color-blue wd-fg-color-white">
+                        <h3>Main content</h3>
+                        <p>
+                            This is the main content. This is the main content. This is the
+                            main content.
+                        </p>
+                    </div>
+                    <div className="wd-grid-col-right-sidebar wd-bg-color-green wd-fg-color-white">
+                        <h3>Side bar</h3>
+                        <p>This is the right sidebar</p>
+                    </div>
+                </div>
             </div>
 
+
             {/* SECTION 3.1.19 - flexible layout */}
-            <div>
-                {/* TODO */}
+            <div id="wd-css-flex">
+                <h2>Flex</h2>
+                <div className="wd-flex-row-container">
+                    <div className="wd-bg-color-yellow
+                    wd-width-75px">
+                        Column 1</div>
+                    <div className="wd-bg-color-blue">
+                        Column 2</div>
+                    <div className="wd-bg-color-red
+                    wd-flex-grow-1">
+                        Column 3</div>
+                </div>
             </div>
+
 
         </div>
     );

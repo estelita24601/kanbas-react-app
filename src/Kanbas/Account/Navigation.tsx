27b7cdd73077414629037201d0f1b4b1ff93b839
Estelita <estelitac24401@hotmail.com>
2024-11-04 20:57:04 -0800

    fix: adding unique keys to each course on the dashboard

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -2,7 +2,6 @@ import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
-    const accountNavigation = ["Signin", "Signup", "Profile"]
     const { currentUser } = useSelector((state: any) => state.accountReducer);
     const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
     const { pathname } = useLocation()
@@ -11,7 +10,7 @@ export default function AccountNavigation() {
         <div className="wd list-group fs-5 rounded-0 me-2">
             {links.map((navOption) => (
                 <Link
-                    to={`/Kanbas/Account/${navOption}`} className={`list-group-item border border-0 ${pathname.includes(navOption) ? `active` : `text-danger`}`}>
+                    to={`/Kanbas/Account/${navOption}`} className={`list-group-item border border-0 ${pathname.includes(navOption) ? `active` : `text-danger`}`} key={`acount-nav-${navOption}`}>
                     {navOption}
                 </Link>)
             )}

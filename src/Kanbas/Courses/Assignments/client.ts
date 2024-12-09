17c87c8d84eed931c48a7881428727bd8b1d51dc
Estelita <24834264+estelita24601@users.noreply.github.com>
2024-12-08 18:19:04 -0800

    client for CRUD operations

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -2,28 +2,27 @@ import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER_A6;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
 
-//get assignments for this course GET
-export const getAssignments = async (courseId: string) => {
-    console.log(`ASSIGNMENT CLIENT: getAssignments ${ASSIGNMENTS_API}/${courseId}`)
-    const response = await axios.get(`${ASSIGNMENTS_API}/${courseId}`);
-    return response.data;
-}
-
-//get one specific assignment using id
-export const getAssignment = async (assignmentId: string, courseId: string) => {
-    console.log(`ASSIGNMENT CLIENT: getAssignment ${ASSIGNMENTS_API}/${courseId}/${assignmentId}`);
-    const response = await axios.get(`${ASSIGNMENTS_API}/${courseId}/${assignmentId}`);
-    return response.data;
-}
-
-//create assignment POST
+/////////////////////////////////// CREATE //////////////////////////////////////////////////
 export const createAssignment = async (assignment: any) => {
     console.log(`ASSIGNMENT CLIENT: createAssignment ${ASSIGNMENTS_API}`)
     const response = await axios.post(`${ASSIGNMENTS_API}`, assignment);
     return response.data;
 }
 
-//update assignment PUT
+/////////////////////////////////// READ //////////////////////////////////////////////////
+export const getAssignments = async (courseId: string) => {
+    console.log(`ASSIGNMENT CLIENT: getAssignments ${ASSIGNMENTS_API}/${courseId}`)
+    const response = await axios.get(`${ASSIGNMENTS_API}/${courseId}`);
+    return response.data;
+}
+
+export const getAssignment = async (assignmentId: string, courseId: string) => {
+    console.log(`ASSIGNMENT CLIENT: getAssignment ${ASSIGNMENTS_API}/${courseId}/${assignmentId}`);
+    const response = await axios.get(`${ASSIGNMENTS_API}/${courseId}/${assignmentId}`);
+    return response.data;
+}
+
+/////////////////////////////////// UPDATE //////////////////////////////////////////////////
 export const replaceAssignment = async (assignmentId: string, assignment: any) => {
     console.log(`ASSIGNMENT CLIENT: replaceAssignment ${ASSIGNMENTS_API}/${assignmentId}\n`);
     //console.log(`${JSON.stringify(assignment, null, 2)}`);
@@ -31,7 +30,7 @@ export const replaceAssignment = async (assignmentId: string, assignment: any) =
     return response.data;
 }
 
-//delete assignment DELETE
+/////////////////////////////////// DELETE //////////////////////////////////////////////////
 export const deleteAssignment = async (assignmentId: string) => {
     console.log(`ASSIGNMENT CLIENT: deleteAssignment ${ASSIGNMENTS_API}/${assignmentId}`);
     const response = await axios.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
